2b49744d414bdc8783eafa4ab902c93e3286b75e
ka-zuto <ishikkiheroku1@onmail.com>
2024-09-28 08:01:22 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,14 +6,14 @@ class Config(object):
     # You can keep this default
     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
     # Put MongoDB URL
-    DATABASE_URL = get_config("DATABASE_URL", "")
+    DATABASE_URL = get_config("DATABASE_URL", "mongodb+srv://hashira:watee123@genshin.kwsyqnl.mongodb.net/?retryWrites=true&w=majority&appName=genshin")
     # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "7571822018:AAGQuvgYtxqMY3LDXtBTiYjPr-LqYygIC5A")
     # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH", "")
-    LOG_CHANNEL = get_config("LOG_CHANNEL")
-    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+    APP_ID = int(get_config("APP_ID", 21861726))
+    API_HASH = get_config("API_HASH", "65788fbdf3e1bbfaa758dc5d5759b1a4")
+    LOG_CHANNEL = -1002353726610
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", "devslab") # Without `@` LOL
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
     AUTH_USERS = set(
@@ -25,7 +25,7 @@ class Config(object):
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = get_config("BOT_USERNAME", "")
+    BOT_USERNAME = get_config("BOT_USERNAME", "MakimaCompressBot")
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
