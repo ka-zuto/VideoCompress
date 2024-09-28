03b5855ba66ddac9c3ae1ddd349da71c2fd48ea4
ka-zuto <ishikkiheroku1@onmail.com>
2024-09-28 08:39:44 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -63,7 +63,7 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             "stackhostbot",
-            api_id=API_ID,
+            api_id=APP_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins={"root": "bot.plugins"},
